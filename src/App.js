c81cdc92794c8f6ff7112603bc1ef2d8d9e28ebf
Raj Kumar <interplaytheheads@gmail.com>
2021-03-11 19:27:58 +0200

    GUI for display table added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Table from 'react-bootstrap/Table';
 import './App.css';
 
 function App() {
@@ -10,36 +12,77 @@ function App() {
 
   useEffect(() => {
     fetch("https://opentdb.com/api.php?amount=1")
-    .then(res => res.json())
-    .then(
-      (result) =>{
-        setIsLoaded(true);
-        setItems(result.results);
-      },
-      (error) => {
-        setIsLoaded(true);
-        setError(error);
-      }
-    )
-  }, [])
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result.results);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
 
-  if(error) {
-    return<div>Error: {error.message}</div>;
-  }
-  else if(!isLoaded) {
-    return<div>Loading...</div>;
-  }
-  else{
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
     return (
-      <ul>
-        {items.map(item =>(
-          <li key={item.id}>
-            {item.category} {item.type} {item.difficulty} {item.question} {item.correct_answer} {item.incorrect_answers}
-          </li>
-        ))}
-      </ul>
-  );
-}
+      <div className="table">
+        <Table>
+          <thead>
+            <tr>
+              <th>Category</th>
+              <th>Type</th>
+              <th>Level</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map((item) => (
+              <tr key={item.id}>
+                <td> {item.category} </td>
+                <td>{item.type}</td>
+                <td>{item.difficulty}</td>
+              </tr>
+            ))}
+          </tbody>
+          {items.map((item) => (
+            <tbody>
+              <tr>
+                <td colSpan="2">Question</td>
+                <td>{item.question}</td>
+              </tr>
+              <tr>
+                <td colSpan="4" align="center">
+                  Answers
+                </td>
+              </tr>
+              <tr>
+                <td colSpan="2" align="center">
+                  {" "}
+                  {item.correct_answer}{" "}
+                </td>
+                <td colSpan="2" align="center">
+                  {item.incorrect_answers[0]}
+                </td>
+              </tr>
+              <tr>
+                <td colSpan="2" align="center">
+                  {item.incorrect_answers[1]}
+                </td>
+                <td colSpan="2" align="center">
+                  {item.incorrect_answers[2]}
+                </td>
+              </tr>
+            </tbody>
+          ))}
+        </Table>
+      </div>
+    );
+  }
 }
 
 export default App;
